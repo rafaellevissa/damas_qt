18968929f5a5add4e594f335cf6f26e2ae10ed58
Rafael Levi <rafaellevissa@hotmail.com>
2016-05-17 00:58:13 -0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "statuswindow.h"
+#include "cfontewindow.h"
+#include "regrawindow.h"
+#include "sobrewindow.h"
+#include "jogadorwindow.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +20,26 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_actionSair_triggered();
+
+    void on_actionScore_triggered();
+
+    void on_actionC_digo_fonte_triggered();
+
+    void on_actionRegras_triggered();
+
+    void on_actionVers_o_triggered();
+
+    void on_actionNovo_jogo_triggered();
+
 private:
     Ui::MainWindow *ui;
+    statusWindow *score;
+    cFonteWindow *cFonte;
+    regraWindow *regra;
+    sobreWindow *sobre;
+    jogadorWindow *jogador;
 };
 
 #endif // MAINWINDOW_H
